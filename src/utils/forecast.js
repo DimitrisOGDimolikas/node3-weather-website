87c428886efae7e8c7d3a27c061734507fa0af75
Dimitris Dimolikas <dimitris.dimolikas@yahoo.com>
2022-01-14 19:58:52 +0200

    Add humidity output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature
-                     + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+                     + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%')
         }
     })
 }
